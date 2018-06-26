965b469f5dbf3f97d86cee3e4b93d15cb37f3ab2
DESKTOP-BRIGC4V\USER <shuna966@gmail.com>
2018-06-26 23:19:09 +0900

    180626

--- MiniBlogApplication.php
+++ MiniBlogApplication.php
@@ -26,6 +26,8 @@ class MiniBlogApplication extends Application
 				=> array('controller'=>'account','action'=>'index'),
 			'account/:action'
 				=> array('controller'=>'account'),
+			'/follow'
+				=> array('controller' => 'account', 'action' => 'follow'),
 		);
 	}
 
